72b0360c08443316c3018f6ba0e17139e40d1835
Fortune Ekeruo <legalmody@gmail.com>
2017-02-22 11:59:59 +0100

    Use template literals and readline close method to exit the fuctions'

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// We import our installed libraries on lines 1 to 3
+// We import our installed libraries from line 1 to 3
 const request = require('request');
 const dotenv  = require('dotenv');
 const readline = require('readline');
 
 // We create a readline interface for reading and writing user's input:
 const rl = readline.createInterface({
-	   input: process.stdin,
-	   output: process.stdout
-	});
+       input: process.stdin,
+       output: process.stdout
+    });
 
 rl.question('Enter a username ', (answer) => {
-	console.log("Yay! We've got a twitter username: ", answer);
-};
+    console.log(`Yay! We've got a twitter username: ${answer}`);
+    rl.close();
+});
 
