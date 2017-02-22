990bb9df853e3822b069e47311458b8da2f489c3
Fortune Ekeruo <legalmody@gmail.com>
2017-02-22 11:12:08 +0100

    Import the required libraries for our application

---
+++ app.js
@@ -0,0 +1,15 @@
+// We import our installed libraries on lines 1 to 3
+const request = require('request');
+const dotenv  = require('dotenv');
+const readline = require('readline');
+
+// We create a readline interface for reading and writing user's input:
+const rl = readline.createInterface({
+	   input: process.stdin,
+	   output: process.stdout
+	});
+
+rl.question('Enter a username ', (answer) => {
+	console.log('Yay! We've got a twitter username: ' answer);
+}	
+
